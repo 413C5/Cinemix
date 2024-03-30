6b2ac3a0e88dc74c94dd282e6629fc4692b09f91
Alejandro González Zárate <124840787+413C5@users.noreply.github.com>
2024-03-29 20:08:35 -0600

    Misc update

--- App_Code/BD.Designer.cs
+++ App_Code/BD.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Documentos\Sexto Semestre\Parcial 2\Soluciones de Programacion Movil\Tareas\Cinemix\Cinemix\App_Code\BD.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Alejandro\Documents\cinemix\App_Code\BD.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
